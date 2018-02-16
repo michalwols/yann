5731399d5f6ea41b47be8a18391650854269136b
michal <michalwols@gmail.com>
2018-02-15 22:00:56 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='yann',
+  version='0.0.1',
+  description='yet another neural network library',
+  url='https://github.com/michalwols/yann',
+  author='Michal',
+  author_email='michal@bite.ai',
+  license='MIT',
+  packages=['yann'],
+  zip_safe=False)
+
