49ad78c8b1abd5879b1941781b57e964995cb2a5
michal <michalwols@gmail.com>
2018-12-29 21:46:49 -0500

    progress

--- yann/utils/__init__.py
+++ yann/utils/__init__.py
@@ -2,6 +2,8 @@ import numpy as np
 import re
 import torch
 from PIL import Image
+import sys
+
 
 
 def camel_to_snake(text):
@@ -28,3 +30,28 @@ def equal(t1, t2):
 
 def randimg(*shape):
   return Image.fromarray((np.random.rand(*shape) * 255).astype('uint8'))
+
+
+
+def progress(it, num=None):
+  if not num:
+    try:
+      num = len(it)
+    except:
+      num = None
+
+  if num:
+    for n, x in enumerate(it, 1):
+      sys.stdout.write(f"\r{n} / {num}")
+      sys.stdout.flush()
+      yield x
+  else:
+    for n, x in enumerate(it, 1):
+      sys.stdout.write(f"\r{n}")
+      sys.stdout.flush()
+      yield x
+
+
+def repeat(val):
+  while True:
+    yield val
