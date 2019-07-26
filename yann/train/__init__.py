e2e95507eec85253347e5ae836a185b6e057a4e9
michal <michalwols@gmail.com>
2019-07-26 19:05:16 -0400

    criterion => loss

--- yann/train/__init__.py
+++ yann/train/__init__.py
@@ -1,21 +1,22 @@
 from .classification import Trainer
+from ..callbacks import Callback
 
 
-def step(model, x, y, optimizer, criterion):
+def step(model, inputs, targets, optimizer, loss, callback: Callback = None):
   model.train()
   optimizer.zero_grad()
 
-  pred = model(x)
-  loss = criterion(pred, y)
+  pred = model(inputs)
+  loss = loss(pred, targets)
 
   loss.backward()
   optimizer.step()
 
-  return x, y, pred, loss
+  return inputs, targets, pred, loss
 
 
-def train(model, batches, optimizer, criterion, device=None):
-  for x, y in batches:
+def train(model, batches, optimizer, loss, device=None, step=step, callback: Callback=None):
+  for inputs, targets in batches:
     if device:
-      x, y = x.to(device), y.to(device)
-    yield step(model, x, y, optimizer, criterion)
+      inputs, targets = inputs.to(device), targets.to(device)
+    yield step(model, inputs, targets, optimizer, loss)
