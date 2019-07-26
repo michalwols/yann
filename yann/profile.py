d5fddfa8e2a1afe360f5174c2ae04ee79abf61e8
michal <michalwols@gmail.com>
2019-07-26 19:06:36 -0400

    comment

--- yann/profile.py
+++ yann/profile.py
@@ -1,5 +1,7 @@
 from torch.autograd.profiler import profile
-
+# TODO:
+# - https://github.com/pytorch/pytorch/issues/3749#issuecomment-374006211
+# - https://pytorch.org/docs/stable/bottleneck.html
 
 def param_count(model):
   return sum(p.numel() for p in model.parameters())
