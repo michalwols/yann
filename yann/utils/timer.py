da380e5a3595b87ff2515359a61911047dd08a66
michal <michalwols@gmail.com>
2019-09-18 17:52:28 -0400

    refactor timer to use a task object for each task and suport cuda synchronization

--- yann/utils/timer.py
+++ yann/utils/timer.py
@@ -1,51 +1,90 @@
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 from contextlib import contextmanager
 
 from datetime import datetime
+import torch.cuda
+
+from ..viz.plot import plot_timeline
+
+
+class Task:
+  __slots__ = ('name', 'start_time', 'end_time', 'meta', 'sync')
+
+  def __init__(self, name=None, start=None, end=None, meta=None, sync=False):
+    self.name = name
+    self.start_time = start
+    self.end_time = end
+    self.meta = meta or {}
+    self.sync = sync
+
+  def start(self, time=None, meta=None, sync=None):
+    if meta:
+      self.meta.update(meta)
+    sync = sync if sync is not None else self.sync
+    if sync and torch.cuda.is_available():
+      torch.cuda.synchronize()
+
+    self.start_time = time or datetime.now()
+
+  def end(self, time=None, meta=None, sync=None):
+    sync = sync if sync is not None else self.sync
+    if sync and torch.cuda.is_available():
+      torch.cuda.synchronize()
+
+    self.end_time = time or datetime.now()
+
+    if meta:
+      self.meta.update(meta)
+
+  @classmethod
+  def begin(cls, name=None, meta=None, sync=None):
+    t = cls(name=name, meta=meta, sync=sync)
+    t.start()
+    return t
+
+  @property
+  def seconds(self):
+    if self.start_time is None or self.end_time is None:
+      return None
+    return (self.end_time - self.start_time).total_seconds()
+
+  def __enter__(self):
+    self.start()
+    return self
+
+  def __exit__(self, exc_type, exc_val, exc_tb):
+    self.end()
 
 
 class Timer:
   def __init__(self, name=None, log=False):
-    self.tasks = OrderedDict()
+    self.tasks = []
     self.name = name
     self.log = log
 
-  def start(self, name, **extra):
-    self.tasks[name] = {
-      'start': datetime.now(),
-      **extra
-    }
+    self.active_tasks = {}
+
+  def start(self, name, sync=True, **meta):
+    task = self.task(name, sync=sync, **meta)
+    if self.log: print('Started', name)
+
+    if task in self.active_tasks:
+      raise ValueError(f'Nesting tasks is not allowed, "{name}" was already started and not finished')
+    self.active_tasks[name] = task
+
+  def end(self, name, sync=True, **meta):
+    task = self.active_tasks.pop(name)
+    if not task:
+      raise ValueError(f"{name} is not an active task so can't be ended")
+    task.end(sync=sync, meta=meta)
 
     if self.log:
-      print('Started', name)
+      print('Ended', task.name, ', took', task.seconds, 'seconds')
 
-  def end(self, name, **extra):
-    task = self.tasks.get(name, {})
-    task['end'] = datetime.now()
-    task = {**extra, **task}
-    self.tasks[name] = task
-
-    if self.log:
-      print('Ended', name, ', took',
-            (task['end'] - task['start']).total_seconds(), 'seconds')
-
-  @contextmanager
-  def task(self, name, **extra):
-    self.start(name, **extra)
-    yield self
-    self.end(name, **extra)
-
-  def summary(self):
-    d = OrderedDict()
-    for name, data in self.tasks.items():
-      start = data.get('start')
-      end = data.get('end')
-
-      x = {**data}
-      if start and end:
-        x['seconds'] = (end - start).total_seconds()
-      d[name] = x
-    return d
+  def task(self, name, sync=True, **meta):
+    task = Task.begin(name=name, meta=meta, sync=sync)
+    self.tasks.append(task)
+    return task
 
   def __enter__(self):
     self.start(self.name)
@@ -53,3 +92,6 @@ class Timer:
 
   def __exit__(self, exc_type, exc_val, exc_tb):
     self.end(self.name)
+
+  def plot(self):
+    plot_timeline(self.tasks)
