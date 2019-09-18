a01a9b32e4198307b4fe8e8c48a7a9ccf95f5934
michal <michalwols@gmail.com>
2019-09-18 17:56:37 -0400

    profile_module to quickly time module forward times

--- yann/utils/profile.py
+++ yann/utils/profile.py
@@ -1,7 +1,38 @@
 from torch.autograd.profiler import profile
+from typing import Union, Tuple, Callable
+import torch
+from .timer import Timer
+
 # TODO:
 # - https://github.com/pytorch/pytorch/issues/3749#issuecomment-374006211
 # - https://pytorch.org/docs/stable/bottleneck.html
 
 def param_count(model):
   return sum(p.numel() for p in model.parameters())
+
+
+def profile_module(
+    module,
+    input,
+    warmup=10,
+    iterations=20,
+    sync=True,
+    timer=None,
+    task_name='iteration',
+    jit=False
+):
+  if jit:
+    import torch.jit
+    module = torch.jit.trace(module, input)
+
+  for n in range(warmup):
+    module(input)
+
+  t = timer or Timer()
+  tasks = []
+  for n in range(iterations):
+    with t.task(task_name, sync=sync) as task:
+      module(input)
+      tasks.append(task)
+
+  return tasks
