0b41eab6d9f6b260d1496c0eab03be910e40dac4
michal <michalwols@gmail.com>
2019-09-25 13:21:13 -0400

    use metaclass for HyperParams and support loading from command line args

--- yann/params.py
+++ yann/params.py
@@ -1,75 +1,130 @@
+from abc import ABCMeta
+from collections import OrderedDict
+from copy import deepcopy
+from .utils import get_arg_parser
+
+class Field:
+  def __init__(self, help=None, type=None, required=True, default=None):
+    self.help = help
+    self.type = type
+    self.required = required
+    self.default = default
+
+  def validate(self, val):
+    if self.type:
+      assert isinstance(val, self.type)
+
+  def __repr__(self):
+    return f"{self.__class__.__name__}(type={self.type}, default={self.default})"
+
+  def __str__(self):
+    return f"{self.__class__.__name__}(type={self.type}, default={self.default})"
 
 
-class HyperParams:
-  # TODO:
-  # track unused variables (warn)
+class Choice(Field):
+  def __init__(self, choices=None, **kwargs):
+    super().__init__(**kwargs)
+    self.choices = choices
+
+  def validate(self, val):
+    super().validate(val)
+    assert val in self.choices
 
 
+class Range(Field):
+  def __init__(self, start, end):
+    super(Range, self).__init__()
+    self.min = min(start, end)
+    self.max = max(start, end)
+
+  def validate(self, val):
+    super().validate(val)
+    assert self.min <= val <= self.max
+
+
+class HyperParamsBase:
   def __init__(self, **args):
-    self.__dict__.update(args)
+    self._change_callbacks = []
+
+    for k, v in args.items():
+      if k in self.__fields__:
+        setattr(self, k, v)
+      else:
+        raise ValueError(f'Unknown parameter: {k}, should be one of {", ".join(self.__fields__)}')
 
   def validate(self):
-    pass
+    for k, f in self.__fields__.items():
+      try:
+        f.validate(getattr(self, k))
+      except Exception as e:
+        raise Exception(f"{k} failed validation. {e}")
 
   @classmethod
-  def from_command(cls, cmd=None, prompt=False, resolve=False):
-    pass
+  def from_command(cls, cmd=None, **kwargs):
+    parser = get_arg_parser(cls.__fields__, **kwargs)
+    parsed = parser.parse_args(cmd.split() if isinstance(cmd, str) else cmd)
+    return cls(**vars(parsed))
 
   @classmethod
-  def from_yaml(cls, path):
-    pass
+  def load(cls, path):
+    if path.endswith(('yaml', 'yml')):
+      import yaml
+      with open(path, 'r') as f:
+        data = yaml.safe_load(f)
+        return cls(**data)
+    elif path.endswith('json'):
+      import json
+      data = json.load(path)
+      return cls(**data)
 
-  def prompt(self, *fields):
-    pass
+  def on_change(self, callback):
+    self._change_callbacks.append(callback)
 
-  def on_change(self):
-    pass
-
-  def on_lookup(self):
-    pass
-
-  def __setattr__(self, key, value):
-    raise AttributeError('Updating properties is not permitted')
+  def __setattr__(self, k, v):
+    if k in self.__fields__:
+      for c in self._change_callbacks:
+        c(k, v)
+    super.__setattr__(self, k, v)
 
   def __getitem__(self, item):
     if isinstance(item, (tuple, list)):
-      return tuple(self.__dict__[k] for k in item)
-    return self.__dict__[item]
+      return tuple(getattr(self, k) for k in item)
+    return getattr(self, item)
+
+  def __setitem__(self, k, v):
+    setattr(self, k, v)
 
   def fork(self, **args):
-    return HyperParams(**{**self.__dict__, **args})
+    return HyperParams(**{**self.items(), **args})
 
   def __repr__(self):
     return (
-      'HyperParams('
-      f"{', '.join(f'{k}={v}' for k, v in self.__dict__.items())}"
+      f'{self.__class__.__name__}('
+      f"{', '.join(f'{k}={v}' for k, v in self.items())}"
       ')'
     )
 
   def __str__(self):
     return (
-        'HyperParams (\n' +
-        ''.join('  {}={}\n'.format(k, v) for k, v in self.__dict__.items()) +
-        ')'
+        f'{self.__class__.__name__}(\n' +
+        ',\n'.join('  {}={}'.format(k, v) for k, v in self.items()) +
+        '\n)'
     )
 
   def __len__(self):
-    return len(self.__dict__)
+    return len(self.keys())
 
   def __hash__(self):
     return hash(tuple(sorted(self.items())))
 
   def keys(self):
-    return self.__dict__.keys()
+    return self.__fields__.keys()
 
   def values(self):
-    return self.__dict__.values()
+    return {getattr(self, k) for k in self.keys()}
 
   def items(self):
-    return self.__dict__.items()
-
-  def grid(self, **args):
-    raise NotImplementedError()
+    return ((k, getattr(self, k)) for k in self.keys())
 
   def inject(self, scope=None, uppercase=True):
     scope = globals() if scope is None else scope
@@ -96,18 +151,42 @@ class HyperParams:
     return hash(self) == hash(other)
 
 
-class Field:
-  def __init__(self, help=None, required=True, none=False):
-    self.help = help
+class MetaHyperParams(ABCMeta):
+  def __new__(metaclass, class_name, bases, namespace):
+    fields = OrderedDict()
 
-class Choices(Field):
+    for base in reversed(bases):
+      if issubclass(base, HyperParamsBase) and base != HyperParamsBase:
+        fields.update(deepcopy(base.__fields__))
+
+    existing_attributes = set(dir(HyperParamsBase)) | set(fields)
+
+    new_attributes = {
+      k: v for (k, v) in namespace.items()
+      if k not in existing_attributes
+         and not k.startswith('_')
+         and not callable(v)
+    }
+
+    for name, annotation in namespace.get('__annotations__', {}).items():
+      if name not in new_attributes:
+        continue
+      if isinstance(annotation, Field):
+        fields[name] = annotation
+      else:
+        fields[name] = Field(type=annotation)
+
+    for name, value in new_attributes.items():
+      if name in fields:
+        fields[name].default = value
+      else:
+        fields[name] = Field(default=value, type=type(value))
+
+    return super().__new__(metaclass, class_name, bases, {
+      '__fields__': fields,
+      **namespace,
+    })
+
+
+class HyperParams(HyperParamsBase, metaclass=MetaHyperParams):
   pass
-
-class Range(Field):
-  def __init__(self, min, max):
-    super(Range, self).__init__()
-    self.min = min
-    self.max = max
-
-  def validate(self, val):
-    assert self.min <= val <= self.max
--- yann/utils/__init__.py
+++ yann/utils/__init__.py
@@ -4,11 +4,44 @@ import torch
 from PIL import Image
 import sys
 
-
-
 def camel_to_snake(text):
   s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', text)
   return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+def abbreviate(text):
+  return re.sub(r"([a-zA-Z])[a-z]*[^A-Za-z]*",r"\1", text).lower()
+
+
+def get_arg_parser(x, description=None, epilog=None, **kwargs):
+    import argparse
+    from ..params import Field
+    parser = argparse.ArgumentParser(description=description, epilog=epilog, **kwargs)
+    for k, v in x.items():
+        if isinstance(v, dict):
+            parser.add_argument(
+                f"-{abbreviate(k)}",
+                f"--{camel_to_snake(k)}",
+                default=v.get('default'),
+                type=v.get('type'),
+                action=v.get('action'),
+                help=v.get('help'),
+                required=v.get('required'),
+                choices=v.get('choices'),
+                dest=v.get('dest')
+            )
+        elif isinstance(v, Field):
+          parser.add_argument(
+            f"-{abbreviate(k)}",
+            f"--{camel_to_snake(k)}",
+            default=v.default,
+            type=v.type,
+            help=f"{v.help or k} (default: {v.default})",
+            required=v.required and v.default is None,
+            choices=getattr(v, 'choices', None),
+          )
+        else:
+            parser.add_argument(f"-{abbreviate(k)}", f"--{camel_to_snake(k)}", default=v, type=type(v))
+    return parser
 
 
 def truthy(items):
