d25453d44647862d61052a3228e5fa421c3a4c09
michal <michalwols@gmail.com>
2019-10-01 14:32:29 -0400

    add batches(), chunk() and shuffle() util functions

--- yann/data/__init__.py
+++ yann/data/__init__.py
@@ -1,3 +1,6 @@
+import torch
+import types
+
 from .classes import Classes
 from .datasets import TransformDataset
 from .loaders import TransformLoader
@@ -12,3 +15,45 @@ def get_dataset_name(x):
     return x.name
 
   return x.__class__.__name__
+
+
+def batches(*tensors, size=32, shuffle=False, order=None):
+  if shuffle:
+    order = torch.randperm(len(tensors[0]))
+
+  if len(tensors) == 1:
+    for i in range(0, len(tensors[0]), size):
+      if order is not None:
+        indices = order[i:i+size]
+        yield tensors[0][indices]
+      else:
+        yield tensors[0][i:i+size]
+  else:
+    for i in range(0, len(tensors[0]), size):
+      if order is not None:
+        indices = order[i:i+size]
+        yield tuple(t[indices] for t in tensors)
+      else:
+        yield tuple(t[i:i+size] for t in tensors)
+
+
+def chunk(sequence, size=32):
+  if isinstance(sequence, types.GeneratorType):
+    batch = []
+    for x in sequence:
+      batch.append(x)
+      if len(batch) == size:
+        yield batch
+        batch = []
+  else:
+    for i in range(0, len(sequence), size):
+      yield sequence[i:i+size]
+
+
+
+def shuffle(*sequences):
+  order = torch.randperm(len(sequences[0]))
+  return (
+     [s[i] for i in order] if isinstance(s, (tuple, list)) else s[order]
+     for s in sequences
+  )
