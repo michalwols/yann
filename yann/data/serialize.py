b8ee62dccf88a660021029fffacb58493149fbdf
michal <michalwols@gmail.com>
2019-10-09 22:41:37 -0400

    LMDB subclasses for specific serialization options

--- yann/data/serialize.py
+++ yann/data/serialize.py
@@ -1,19 +1,26 @@
 import pyarrow
 
 
-pyarrow.serialization.register_torch_serialization_handlers(
-  pyarrow.serialization.default_serialization_context()
-)
+ctx = pyarrow.serialization.default_serialization_context()
+
+def enable_torch_serialization(context=ctx):
+  pyarrow.serialization.register_torch_serialization_handlers(
+    context
+  )
+
+# enable by default
+enable_torch_serialization()
 
 
-def deserialize_arrow(buf):
-  return pyarrow.deserialize(buf)
+def deserialize_arrow(buf, context=ctx):
+  return pyarrow.deserialize(buf, context)
 
 
-def serialize_arrow(x):
-  return pyarrow.serialize(x).to_buffer()
+def serialize_arrow(x, context=ctx):
+  return pyarrow.serialize(x, context=context).to_buffer()
 
 
+# use arrow as default serialization method
 serialize = serialize_arrow
 deserialize = deserialize_arrow
 
--- yann/data/storage/lmdb.py
+++ yann/data/storage/lmdb.py
@@ -1,15 +1,15 @@
 import lmdb
+import pickle
 
-from ..serialize import serialize, deserialize
+from ..serialize import serialize_arrow, deserialize_arrow, to_bytes, to_unicode
+from ..images import image_to_bytes, image_from_bytes
 
 
 class LMDB:
-  NONE = '__LMDB_NONE__'
-
-  def __init__(self, path, **kwargs):
+  def __init__(self, path, map_size=1e10, **kwargs):
     self.path = path
     self.db = None
-    self.open(**kwargs)
+    self.open(map_size=map_size, **kwargs)
 
   @property
   def stats(self):
@@ -30,29 +30,97 @@ class LMDB:
 
   def __getitem__(self, key):
     with self.db.begin(write=False) as t:
-      return self.deserialize(t.get(self.serialize(key)))
+      return self.deserialize(t.get(self.serialize_key(key)))
 
   def __setitem__(self, key, value):
     with self.db.begin(write=True) as t:
-      return t.put(*self.serialize(key, value))
+      return t.put(self.serialize_key(key), self.serialize(value))
 
   def __delitem__(self, key):
     with self.db.begin(write=True) as t:
-      return t.delete(self.serialize(key))
+      return t.delete(self.serialize_key(key))
 
   def __iter__(self):
     with self.db.begin(write=False) as t:
       for k, v in t.cursor():
-        yield self.deserialize(k, v)
+        yield self.deserialize_key(k), self.deserialize(v)
 
-  def serialize(self, key, value=NONE):
-    if value is LMDB.NONE:
-      return key
-    else:
-      return key, value
+  def update(self, items):
+    if isinstance(items, dict):
+      items = items.items()
+    with self.db.begin(write=True) as t:
+      for k, v in items:
+        t.put(self.serialize_key(k), self.serialize(v))
 
-  def deserialize(self, key, value=NONE):
-    if value is LMDB.NONE:
-      return key
-    else:
-      return key, value
+  @staticmethod
+  def serialize_key(x):
+    return x
+
+  @staticmethod
+  def deserialize_key(x):
+    return x
+
+  @staticmethod
+  def serialize(x):
+    return x
+
+  @staticmethod
+  def deserialize(x):
+    return x
+
+
+class ArrowLMDB(LMDB):
+  """
+  LMDB that uses arrow to serialize the values
+  """
+
+  @staticmethod
+  def serialize_key(x): 
+    return to_bytes(x)
+
+  @staticmethod
+  def deserialize_key(x): 
+    return to_unicode(x)
+
+  @staticmethod
+  def serialize(x): 
+    return serialize_arrow(x)
+
+  @staticmethod
+  def deserialize(x): 
+    return deserialize_arrow(x)
+
+
+class PickleLMDB(LMDB):
+  @staticmethod
+  def serialize_key(x):
+    return to_bytes(x)
+
+  @staticmethod
+  def deserialize_key(x):
+    return to_unicode(x)
+
+  @staticmethod
+  def serialize(x):
+    return pickle.dumps(x, protocol=-1)
+
+  @staticmethod
+  def deserialize(x):
+    return pickle.loads(x)
+
+
+
+class ImageLMDB(LMDB):
+  format = 'jpeg'
+
+  def serialize_key(self, x):
+    return to_bytes(x)
+
+  def deserialize_key(self, x):
+    return to_unicode(x)
+
+  def serialize(self, x):
+    return image_to_bytes(x, format=self.format)
+
+  def deserialize(self, x):
+    return image_from_bytes(x)
