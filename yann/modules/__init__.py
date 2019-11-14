5686cdfdf59dcc5cd4f1d9a1d442183238d01c4d
michal <michalwols@gmail.com>
2019-11-14 00:31:43 -0500

    bring common modules into yann.modules

--- yann/modules/__init__.py
+++ yann/modules/__init__.py
@@ -1,5 +1,18 @@
-from .shape import View, Flatten, Infer
+from torch import nn
+
+from .conv import (
+  ConvBlock,
+  ConvBlock1x1,
+  ConvBlock3x3,
+  MixConv,
+  SqueezeExcitation,
+  EfficientChannelAttention,
+  DepthwiseSeparableConv2d,
+  DepthwiseConv2d
+)
+from .shape import View, Flatten, Infer, Squeeze, Reshape, Permute, Transpose, FlattenSequences
 from .stack import Stack
+from .residual import Residual
 
 
 class Init:
@@ -10,3 +23,17 @@ class Init:
 
   def __call__(self, *args, **kwargs):
     return self.cls(*self.args, *args, **{**self.kwargs, **kwargs})
+
+
+
+class TrainEvalSwitch(nn.Module):
+  def __init__(self, train=None, eval=None):
+    super().__init__()
+    self.train = train
+    self.eval = eval
+
+  def forward(self, *args):
+    if self.training:
+      return self.train(*args) if self.train else args
+    else:
+      return self.eval(*args) if self.eval else args
---
+++ yann/modules/conv/__init__.py
@@ -0,0 +1,5 @@
+from .mixconv import MixConv
+from .conv import ConvBlock, ConvBlock1x1, ConvBlock3x3
+from .depthwise import DepthwiseConv2d, DepthwiseSeparableConv2d
+from .squeeze_excitation import SqueezeExcitation
+from .attention import EfficientChannelAttention
