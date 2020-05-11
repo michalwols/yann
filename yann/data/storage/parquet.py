9b3d47a4ae0e3f216748f4ff376e9ac6c12052c8
michal <michalwols@gmail.com>
2020-05-11 00:12:18 -0400

    batch readers and writers with parquet support

--- yann/data/storage/parquet.py
+++ yann/data/storage/parquet.py
@@ -2,6 +2,7 @@ from pyarrow import parquet as pq
 import pyarrow as pa
 import pandas as pd
 
+from .batch_files import BatchWriter
 
 def write_parquet(dest, data, columns=None, **kwargs):
   if isinstance(data, dict):
@@ -20,4 +21,35 @@ def write_parquet(dest, data, columns=None, **kwargs):
 
 
 def read_parquet():
+  pass
+
+
+class BatchParquetFileWriter(BatchWriter):
+  def __init__(self, path, schema=None, encoders=None, names=None, meta=None, **writer_args):
+    super(BatchParquetFileWriter, self).__init__(
+      path=path,
+      batches_per_file=1,  # writing to a parquet
+      encoders=encoders,
+      names=names,
+      meta=meta
+    )
+
+    self.path = path
+    self.schema = schema
+    self._writer_args = writer_args
+
+    # will determine schema on first write if not provided
+    if self.schema:
+      self.writer= pq.ParquetWriter(path, self.schema, **self._writer_args)
+
+  def _write(self):
+    df = pd.DataFrame(self.collate(self.buffers))
+    table = pa.Table.from_pandas(df)
+    if self.writer is None:
+      self.schema = table.schema
+      self.writer = pq.ParquetWriter(self.path, self.schema, **self._writer_args)
+    self.writer.write_table(table)
+
+
+class BatchParquetDatasetWriter(BatchWriter):
   pass
