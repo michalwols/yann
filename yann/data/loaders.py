f6c1ae3059afa5158de8c1e88168c72300eee47e
michal <michalwols@gmail.com>
2020-09-01 23:27:47 -0400

    don't reinitialize dataloader iter after every epoch

--- yann/data/loaders.py
+++ yann/data/loaders.py
@@ -1,8 +1,41 @@
-from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, Dataset
+from torch.utils.data import DataLoader as TorchLoader, RandomSampler, SequentialSampler, Dataset
 from typing import Union, Iterable, Optional, Callable
 
 from ..datasets import TransformDataset
 import yann
+
+
+class DataLoader(TorchLoader):
+  """
+  Reuse the same iterator for multiple epochs to avoid startup penalty of
+  initializing it each time
+
+  (inspired by loader from `timm`)
+
+  # might be fixed here https://github.com/pytorch/pytorch/pull/35795
+  """
+  def __init__(self, *args, **kwargs):
+    super(DataLoader, self).__init__(*args, **kwargs)
+    self.__initialized = False
+    self.batch_sampler = LoopSampler(self.batch_sampler)
+    self.__initialized = True
+    self.iterator = super().__iter__()
+
+  def __len__(self):
+    return len(self.batch_sampler.sampler)
+
+  def __iter__(self):
+    for i in range(len(self)):
+      yield next(self.iterator)
+
+
+class LoopSampler:
+  def __init__(self, sampler):
+    self.sampler = sampler
+
+  def __iter__(self):
+    while True:
+      yield from self.sampler
 
 
 class TransformLoader(DataLoader):
@@ -12,7 +45,7 @@ class TransformLoader(DataLoader):
 
 
 def loader(
-  data: Union[str, Iterable, Dataset, DataLoader],
+  data: Union[str, Iterable, Dataset, TorchLoader],
   transform: Optional[Callable] = None,
   **kwargs
 ):
