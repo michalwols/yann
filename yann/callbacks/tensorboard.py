b8ceabd3127257169f5d5dcaff3aaf88cc7a824d
michal <michalwols@gmail.com>
2020-09-02 23:49:50 -0400

    tensorboard callback metrics

--- yann/callbacks/tensorboard.py
+++ yann/callbacks/tensorboard.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from torch.utils.tensorboard import SummaryWriter
 from torch import nn
 
@@ -5,6 +7,7 @@ from .base import Callback
 
 
 class Tensorboard(Callback):
+  writer: SummaryWriter
   def __init__(self, root=None, trainer=None, writer=None):
     self.root = root
     self.trainer = trainer
@@ -12,15 +15,18 @@ class Tensorboard(Callback):
 
     self.logged_model_graph = False
 
+  def _init_writer(self):
+    self.writer = SummaryWriter(self.root)
+
   def on_train_start(self, trainer=None, **kwargs):
     if self.root is None:
       self.root = trainer.paths.tensorboard
     self.trainer = trainer
 
     if self.writer is None:
-      self.writer = SummaryWriter(self.root)
+      self._init_writer()
 
-    if self.trainer.params:
+    if self.trainer and self.trainer.params:
       self.writer.add_hparams(dict(self.trainer.params), {})
 
   def on_batch_start(self, inputs=None, **kwargs):
@@ -28,18 +34,44 @@ class Tensorboard(Callback):
       self.writer.add_graph(self.trainer.model, inputs)
       self.logged_model_graph = True
 
+  def on_batch_end(
+    self,
+    batch=None,
+    inputs=None,
+    targets=None,
+    outputs=None,
+    loss=None,
+    trainer=None
+  ):
+    self.writer.add_scalar('train/loss', loss, global_step=trainer.num_steps)
+    for metric, values in trainer.history.metrics.items():
+      self.writer.add_scalar(f'train/{metric}', values[-1], global_step=len(values) - 1)
+
+  def on_validation_end(self, targets=None, outputs=None, loss=None, trainer=None):
+    for metric, values in trainer.history.val_metrics.items():
+      self.writer.add_scalar(f'validation/{metric}', values[-1], global_step=len(values) - 1)
+
   def log(self, val, tag=None, step=None, **kwargs):
     if step is None and self.trainer:
       step = self.trainer.num_steps
 
     if isinstance(val, nn.Module):
-      self.writer.add_graph(val, kwargs.get('input'), verbose=kwargs.get('verbose', False))
+      self.writer.add_graph(
+        val,
+        kwargs.get('input'),
+        verbose=kwargs.get('verbose', False)
+      )
 
-  def show(self):
+  def show(self, root=None):
     from IPython import get_ipython
     ipython = get_ipython()
     ipython.magic('load_ext tensorboard')
-    ipython.magic(f'tensorboard --logdir {self.root}')
+
+    if isinstance(self, (str, Path)):
+      # static method
+      ipython.magic(f'tensorboard --logdir {self}')
+    else:
+      ipython.magic(f'tensorboard --logdir {root or self.root}')
 
   def close(self):
     if self.writer:
@@ -48,4 +80,3 @@ class Tensorboard(Callback):
   def flush(self):
     if self.writer:
       self.writer.flush()
-
