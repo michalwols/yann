2edfb2731fd785cae2337d1241d815c053328afd
michal <michalwols@gmail.com>
2020-10-20 23:17:13 -0400

    image collate

--- yann/data/collate.py
+++ yann/data/collate.py
@@ -1,5 +1,7 @@
+import numpy as np
 import torch
 from torch.utils.data.dataloader import default_collate
+
 from .utils import pad_to_largest
 
 
@@ -20,9 +22,10 @@ class PadCollate:
     return torch.stack(inputs), torch.stack(targets)
 
 
-
 class FilterCollate:
-  def __init__(self, filter=None, collate=default_collate, value=None):
+  def __init__(
+    self, filter=None, collate=default_collate, value=None
+  ):
     self.filter = filter or (
       lambda items: [it for it in items if it is not value]
     )
@@ -36,3 +39,27 @@ class FilterCollate:
 filter_collate = FilterCollate()
 
 
+def image_collate(batch, memory_format=torch.contiguous_format):
+  """
+  Note: using tensor cores channels_last should lead to better performance
+    inspired by: https://github.com/NVIDIA/apex/blob/master/examples/imagenet/main_amp.py#L28
+  Args:
+    batch: [(image, target), ...]
+  """
+  images, targets = zip(*batch)
+  targets = torch.tensor(targets, dtype=torch.int64)
+
+  w, h = images[0].shape[:2]
+
+  tensor = torch.zeros(
+    (len(images), 3, h, w),
+    dtype=torch.uint8,
+  ).contiguous(memory_format=memory_format)
+
+  for i, img in enumerate(images):
+    nump_array = np.asarray(img, dtype=np.uint8)
+    if (nump_array.ndim < 3):
+      nump_array = np.expand_dims(nump_array, axis=-1)
+    nump_array = np.rollaxis(nump_array, 2)
+    tensor[i] += torch.from_numpy(nump_array)
+  return tensor, targets
