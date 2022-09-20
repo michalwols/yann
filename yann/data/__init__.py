360e74ac43e3949bbb964e612ab658a1cff1a51f
Michal <michalwols@gmail.com>
2022-09-20 13:19:11 -0400

    default get_name function

--- yann/data/__init__.py
+++ yann/data/__init__.py
@@ -6,6 +6,13 @@ from .loaders import TransformLoader
 from .transform import Transformer
 
 
+def get_name(x):
+  if hasattr(x, 'name'):
+    return x.name
+
+  return x.__class__.__name__
+
+
 def get_dataset_name(x):
   while hasattr(x, 'dataset'):
     x = x.dataset
