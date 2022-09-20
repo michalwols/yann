d74828169d90c770436a82f120d76f69e51d4ba2
Michal <michalwols@gmail.com>
2022-09-20 13:20:07 -0400

    csv and parquet support

--- yann/data/io/__init__.py
+++ yann/data/io/__init__.py
@@ -6,6 +6,8 @@ from collections import namedtuple
 import csv
 import gzip
 from pathlib import Path
+from typing import Union
+
 import torch
 
 
@@ -64,6 +66,20 @@ class Loader:
 load = Loader()
 
 
+def to_pyarrow_table(x):
+  import pyarrow as pa
+  try:
+    import pandas as pd
+    if isinstance(x, pd.DataFrame):
+      x = pa.Table.from_pandas(x)
+  except ImportError:
+    pass
+
+  if not isinstance(x, pa.Table):
+    raise ValueError(f'unsupported type {type(x)}')
+
+  return x
+
 class Saver:
   def __call__(
     self, x, path, format=None, serialize=None, filesystem=None, **kwargs
@@ -74,6 +90,10 @@ class Saver:
       return getattr(self, format)(x, path, **kwargs)
     raise ValueError(f'File format not supported ({format})')
 
+  def txt(self, x, path):
+    with open(path, 'w') as f:
+      f.write(x)
+
   def th(self, x, path, **kwargs):
     return torch.save(x, path, **kwargs)
 
@@ -83,7 +103,28 @@ class Saver:
   def yaml(self, x, path, **kwargs):
     import yaml
     with open(path, 'w') as f:
-      yaml.dump(x, f)
+      yaml.dump(x, f, sort_keys=False)
+
+  def csv(self, x, path, **kwargs):
+    import pyarrow.csv as csv
+    x = to_pyarrow_table(x)
+    csv.write_csv(x, path, **kwargs)
+
+  def parquet(
+      self, x: Union['pandas.Dataframe', 'pyarrow.Table'],
+      path,
+      **kwargs
+  ):
+    import pyarrow.parquet as pq
+    import pyarrow as pa
+
+    x = to_pyarrow_table(x)
+
+    if isinstance(x, pa.Table):
+      pq.write_table(x, path, **kwargs)
+    else:
+      raise ValueError(f'Unsupported type {type(x)} expected pandas.Dataframe or pyarrow.Table')
+
 
 
   def pickle(self, x, path, **kwargs):
