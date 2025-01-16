79199108e56c88601742548498d4250174b5ab5b
Michal Wolski <michalwols@gmail.com>
2025-01-16 07:53:34 -0500

    update tests runner

--- tests/data/test_place.py
+++ tests/data/test_place.py
@@ -1,11 +1,18 @@
+import torch
+import pytest
+
 from yann.data.place import Place
 
 
-def test_place():
+devices = ['cpu', 'cuda'] if torch.cuda.is_available() else ['cpu']
+
+
+@pytest.mark.parametrize('device', devices)
+def test_place(device):
   import torch
   tuple_batch = (torch.rand(3,3), torch.rand(3,1), 'foo')
 
-  place = Place(('cpu', 'cuda'))
+  place = Place(('cpu', device))
   b = place(tuple_batch)
   # assert b[0].device == 'cpu'
   assert b[2] == 'foo'
